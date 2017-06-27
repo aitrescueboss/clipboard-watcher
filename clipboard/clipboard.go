dd9b5caa5f94bc42cb99286f1bb56006a88695a3
Kazuo SHOUJI <aitrescueboss@gmail.com>
2017-06-27 22:37:28 +0900

    さてうまくいくか

--- clipboard/clipboard.go
+++ clipboard/clipboard.go
@@ -10,14 +10,17 @@ import (
 func GetClipBoard() ([]byte, error) {
   
   tCommandLiteral := ""
+  tCommandArgs := make([]string, 0, 8)
   switch runtime.GOOS {
   case "darwin":
     tCommandLiteral = "/usr/bin/pbpaste"
   case "linux":
-    tCommandLiteral = "xsel --clipboard --output"
+    tCommandLiteral = "xsel"
+    tCommandArgs = append(tCommandArgs, "--clipboard")
+    tCommandArgs = append(tCommandArgs, "--output")
   }
   
-  tPasteCommand := exec.Command(tCommandLiteral)
+  tPasteCommand := exec.Command(tCommandLiteral, tCommandArgs...)
   tText, tError := tPasteCommand.Output()
   
   if tError != nil {
@@ -39,20 +42,26 @@ func PasteToClipBoard(aRawBytes []byte) error {
   tTempFile.Sync()
   tTempFile.Close()
   
-  tCommandLiteral := ""
+  tCommandLiteral := "/bin/bash"
+  tCommandArgs := make([]string, 0, 8)
+  tCommandArgs = append(tCommandArgs, "-c")
   switch runtime.GOOS {
   case "darwin":
-    tCommandLiteral = "/usr/bin/pbcopy"
+    tCommandArgs = append(tCommandArgs, fmt.Sprintf("/bin/cat \"%s\" | %s", tTempFilePath, "/usr/bin/pbcopy"))
+    //tCommandArgs = append(tCommandArgs, "/usr/bin/pbcopy")
+    //tCommandLiteral = "/usr/bin/pbcopy"
   case "linux":
-    tCommandLiteral = "xsel --clipboard --input"
+    tCommandLiteral = "xsel"
+    tCommandArgs = append(tCommandArgs, fmt.Sprintf("/bin/cat \"%s\" | %s %s %s", tTempFilePath, "xsel", "--clipboard", "--input"))
+    //tCommandArgs = append(tCommandArgs, "--clipboard")
+    //tCommandArgs = append(tCommandArgs, "--input")
   }
   
   // このあたりを参考にするとパイプ経由でコマンドの実行結果を得られる
   // http://qiita.com/yuroyoro/items/9358cd25b5f7fe9dd37f
   tCopyCommand := exec.Command(
-    "/bin/bash",
-    "-c",
-    fmt.Sprintf("/bin/cat \"%s\" | %s", tTempFilePath, tCommandLiteral),
+    tCommandLiteral,
+    tCommandArgs...,
   )
   _, tError = tCopyCommand.Output()
   if tError != nil {
